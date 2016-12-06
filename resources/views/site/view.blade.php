987d55087fcdee8a40a5f009e18ab01363d8ebb9
TRAN DUY <TRAN DUY>
2016-12-07 00:03:51 +0700

    update

--- resources/views/site/view.blade.php
+++ resources/views/site/view.blade.php
@@ -3,6 +3,9 @@
 @section('keywords'){!! $article->meta_keyword?$article->meta_keyword:'' !!}@endsection
 @section('description'){!! $article->meta_description?$article->meta_description:'' !!}@endsection
 @section('content')
+
+    <script src="{{asset('assets/site/js/jssor.slider-22.0.3.min.js')}}"></script>
+
     <div id="section-body">
         <!--start compare panel-->
         <div id="compare-controller" class="compare-panel">
@@ -14,53 +17,282 @@
         </div>
         <!--end compare panel-->
         <!--start detail top-->
-        <section class="detail-top detail-top-grid">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="header-detail table-list">
-                            <div class="header-left">
-                                <ol class="breadcrumb">
-                                    <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a itemprop="url" href="/"><i class="fa fa-home"></i></a></li>
-                                    <li><a href="{{ route('danhmuc',$article->category->slug) }}">{{$article->category->title}}</a></li>
-                                    <li class="active">{{$article->title}}</li>
-                                </ol>
-                                <h1>{{$article->title}}
-                                    <span class="label-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="header-detail table-list">
+                        <div class="header-left">
+                            <ol class="breadcrumb">
+                                <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a itemprop="url" href="/"><i class="fa fa-home"></i></a></li>
+                                <li><a href="{{ route('danhmuc',$article->category->slug) }}">{{$article->category->title}}</a></li>
+                                <li class="active">{{$article->title}}</li>
+                            </ol>
+                            <h1>{{$article->title}}
+                                <span class="label-wrap">
                                     <span class="label label-primary label-status-7"><a style="color: white" href="{{ route('danhmuc',$article->category->slug) }}">{{$article->category->title}}</a></span></span>
-                                </h1>
-                                <address class="property-address"><i class="fa fa-map-marker"></i> {{$article->location}}</address>
-                            </div>
-                            <div class="header-right">
-                                <ul class="actions">
-                                    <li class="share-btn">
-                                        <div class="fb-share-button" data-href="{{route('view',$article->slug.'-'.$article->id)}}" data-layout="button_count" data-size="small" data-mobile-iframe="true">
-                                            <a class="fb-xfbml-parse-ignore" target="_blank" href="{{route('view',$article->slug.'-'.$article->id)}}">Chiasẻ</a></div>
-                                    </li>
-                                </ul>
-                                <span class="item-price">{{$article->money}}</span></div>
+                            </h1>
+                            <address class="property-address"><i class="fa fa-map-marker"></i> {{$article->location}}</address>
                         </div>
-                        <div class="detail-media">
-                            <div class="tab-content">
-                                <div id="gallery" class="tab-pane fade in active" style="background-image: url({{$article->picture_1}})">
-                                    <a href="#" class="popup-trigger">
-                                    </a>
+                        <div class="header-right">
+                            <ul class="actions">
+                                <li class="share-btn">
+                                    <div class="fb-share-button" data-href="{{route('view',$article->slug.'-'.$article->id)}}" data-layout="button_count" data-size="small" data-mobile-iframe="true">
+                                        <a class="fb-xfbml-parse-ignore" target="_blank" href="{{route('view',$article->slug.'-'.$article->id)}}">Chiasẻ</a></div>
+                                </li>
+                            </ul>
+                            <span class="item-price">{{$article->money}}</span></div>
+                    </div>
+                    <div class="detail-media">
+                        <div class="tab-content">
+                            <script type="text/javascript">
+                                jssor_1_slider_init = function () {
+
+                                    var jssor_1_options = {
+                                        $AutoPlay: true,
+                                        $ArrowNavigatorOptions: {
+                                            $Class: $JssorArrowNavigator$
+                                        },
+                                        $ThumbnailNavigatorOptions: {
+                                            $Class: $JssorThumbnailNavigator$,
+                                            $Cols: 4,
+                                            $SpacingX: 4,
+                                            $SpacingY: 4,
+                                            $Orientation: 2,
+                                            $Align: 0
+                                        }
+                                    };
+
+                                    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+                                    /*responsive code begin*/
+                                    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+                                    function ScaleSlider() {
+                                        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                                        if (refSize) {
+                                            refSize = Math.min(refSize, 1200);
+                                            jssor_1_slider.$ScaleWidth(refSize);
+                                        }
+                                        else {
+                                            window.setTimeout(ScaleSlider, 500);
+                                        }
+                                    }
+
+                                    ScaleSlider();
+                                    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+                                    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+                                    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+                                    /*responsive code end*/
+                                };
+                            </script>
+                            <style>
+                                /* jssor slider arrow navigator skin 02 css */
+                                /*
+                                .jssora02l                  (normal)
+                                .jssora02r                  (normal)
+                                .jssora02l:hover            (normal mouseover)
+                                .jssora02r:hover            (normal mouseover)
+                                .jssora02l.jssora02ldn      (mousedown)
+                                .jssora02r.jssora02rdn      (mousedown)
+                                .jssora02l.jssora02lds      (disabled)
+                                .jssora02r.jssora02rds      (disabled)
+                                */
+                                .jssora02l, .jssora02r {
+                                    display: block;
+                                    position: absolute;
+                                    /* size of arrow element */
+                                    width: 55px;
+                                    height: 55px;
+                                    cursor: pointer;
+                                    background: url('/assets/img/a02.png') no-repeat;
+                                    overflow: hidden;
+                                }
+
+                                .jssora02l {
+                                    background-position: -3px -33px;
+                                }
+
+                                .jssora02r {
+                                    background-position: -63px -33px;
+                                }
+
+                                .jssora02l:hover {
+                                    background-position: -123px -33px;
+                                }
+
+                                .jssora02r:hover {
+                                    background-position: -183px -33px;
+                                }
+
+                                .jssora02l.jssora02ldn {
+                                    background-position: -3px -33px;
+                                }
+
+                                .jssora02r.jssora02rdn {
+                                    background-position: -63px -33px;
+                                }
+
+                                .jssora02l.jssora02lds {
+                                    background-position: -3px -33px;
+                                    opacity: .3;
+                                    pointer-events: none;
+                                }
+
+                                .jssora02r.jssora02rds {
+                                    background-position: -63px -33px;
+                                    opacity: .3;
+                                    pointer-events: none;
+                                }
+
+                                /* jssor slider thumbnail navigator skin 11 css *//*.jssort11 .p            (normal).jssort11 .p:hover      (normal mouseover).jssort11 .pav          (active).jssort11 .pav:hover    (active mouseover).jssort11 .pdn          (mousedown)*/
+                                .jssort11 .p {
+                                    position: absolute;
+                                    top: 0;
+                                    left: 0;
+                                    width: 200px;
+                                    height: 69px;
+                                    background: #181818;
+                                }
+
+                                .jssort11 .tp {
+                                    position: absolute;
+                                    top: 0;
+                                    left: 0;
+                                    width: 100%;
+                                    height: 100%;
+                                    border: none;
+                                }
+
+                                .jssort11 .i, .jssort11 .pav:hover .i {
+                                    position: absolute;
+                                    top: 3px;
+                                    left: 3px;
+                                    width: 60px;
+                                    height: 30px;
+                                    border: white 1px dashed;
+                                }
+
+                                * html .jssort11 .i {
+                                    width /**/: 62px;
+                                    height /**/: 32px;
+                                }
+
+                                .jssort11 .pav .i {
+                                    border: white 1px solid;
+                                }
+
+                                .jssort11 .t, .jssort11 .pav:hover .t {
+                                    position: absolute;
+                                    top: 3px;
+                                    left: 68px;
+                                    width: 129px;
+                                    height: 32px;
+                                    line-height: 32px;
+                                    text-align: center;
+                                    color: #fc9835;
+                                    font-size: 13px;
+                                    font-weight: 700;
+                                }
+
+                                .jssort11 .pav .t, .jssort11 .p:hover .t {
+                                    color: #fff;
+                                }
+
+                                .jssort11 .c, .jssort11 .pav:hover .c {
+                                    position: absolute;
+                                    top: 38px;
+                                    left: 3px;
+                                    width: 194px;
+                                    height: 32px;
+                                    line-height: 32px;
+                                    color: #fff;
+                                    font-size: 11px;
+                                    font-weight: 400;
+                                    overflow: hidden;
+                                }
+
+                                .jssort11 .pav .c, .jssort11 .p:hover .c {
+                                    color: #fc9835;
+                                }
+
+                                .jssort11 .t, .jssort11 .c {
+                                    transition: color 2s;
+                                    -moz-transition: color 2s;
+                                    -webkit-transition: color 2s;
+                                    -o-transition: color 2s;
+                                }
+
+                                .jssort11 .p:hover .t, .jssort11 .pav:hover .t, .jssort11 .p:hover .c, .jssort11 .pav:hover .c {
+                                    transition: none;
+                                    -moz-transition: none;
+                                    -webkit-transition: none;
+                                    -o-transition: none;
+                                }
+
+                                .jssort11 .p:hover, .jssort11 .pav:hover {
+                                    background: #333;
+                                }
+
+                                .jssort11 .pav, .jssort11 .p.pdn {
+                                    background: #462300;
+                                }
+                            </style>
+                            <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 810px; height: 3h00px; overflow: hidden; visibility: hidden; background-color: #000000;">
+                                <!-- Loading Screen -->
+                                <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+                                    <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+                                    <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
                                 </div>
+                                <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 600px; height: 300px; overflow: hidden;">
+                                    @if($article->picture_1)
+                                        <div data-p="112.50">
+                                            <img data-u="image" src="{{show_content_picture($article->picture_1)}}"/>
+                                        </div>
+                                    @endif
+                                    @if($article->picture_2)
+                                        <div data-p="112.50">
+                                            <img data-u="image" src="{{show_content_picture($article->picture_2)}}"/>
+                                        </div>
+                                    @endif
+                                    @if($article->picture_3)
+                                        <div data-p="112.50">
+                                            <img data-u="image" src="{{show_content_picture($article->picture_3)}}"/>
+                                        </div>
+                                    @endif
+                                    @if($article->picture_4)
+                                        <div data-p="112.50">
+                                            <img data-u="image" src="{{show_content_picture($article->picture_4)}}"/>
+                                        </div>
+                                    @endif
+                                    @if($article->picture_5)
+                                        <div data-p="112.50">
+                                            <img data-u="image" src="{{show_content_picture($article->picture_5)}}"/>
+                                        </div>
+                                    @endif
+                                </div>
+                                <!-- Thumbnail Navigator -->
+                                <div data-u="thumbnavigator" class="jssort11"
+                                     style="position:absolute;right:5px;top:0px;font-family:Arial, Helvetica, sans-serif;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;width:200px;height:300px;"
+                                     data-autocenter="2">
+                                    <!-- Thumbnail Item Skin Begin -->
+                                    <div data-u="slides" style="cursor: default;">
+                                        <div data-u="prototype" class="p">
+                                            <div data-u="thumbnailtemplate" class="tp"></div>
+                                        </div>
+                                    </div>
+                                    <!-- Thumbnail Item Skin End -->
+                                </div>
+                                <!-- Arrow Navigator -->
+                                <span data-u="arrowleft" class="jssora02l" style="top:0px;left:8px;width:55px;height:55px;" data-autocenter="2"></span>
+                                <span data-u="arrowright" class="jssora02r" style="top:0px;right:218px;width:55px;height:55px;" data-autocenter="2"></span>
                             </div>
-                            <div class="media-tabs">
-                                <ul class="media-tabs-list">
-                                    <li class="popup-trigger" data-placement="bottom" data-toggle="tooltip" data-original-title="View Photos">
-                                        <a href="#gallery" data-toggle="tab">
-                                            <i class="fa fa-camera"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+                            <script type="text/javascript">jssor_1_slider_init();</script>
+                            <!-- #endregion Jssor Slider End -->
                         </div>
                     </div>
                 </div>
             </div>
-        </section>    <!--end detail top-->
+        </div>
+        <!--end detail top-->
         <!--start detail content-->
         <section class="section-detail-content" style="transform: none;">
             <div class="container" style="transform: none;">
